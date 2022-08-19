0b1a31b1543cde61012ec4c6da3c702586d3d462
jbmoneysweg <87237420+jbmoneysweg@users.noreply.github.com>
2022-08-18 20:31:50 -0400

    Update tester.php

--- tester.php
+++ tester.php
@@ -38,7 +38,7 @@ if (isset($_GET['action']))
         /*Insert data.*/  
         $insertSql = "INSERT INTO empTable (emp_id,name,education,email)   
 VALUES (?,?,?,?)";  
-        $params = array($ip, $locations, $visit, $pgOne]  
+        $params = array($ip, $locations, $visit, $pgOne  
         );  
         $stmt = sqlsrv_query($conn, $insertSql, $params);  
         if ($stmt === false)  
