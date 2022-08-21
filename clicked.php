5cacc81d0ba33fdc22d2952c0c322fd17a268ce9
jbmoneysweg <87237420+jbmoneysweg@users.noreply.github.com>
2022-08-21 16:16:48 -0400

    Add files via upload

---
+++ clicked.php
@@ -0,0 +1,55 @@
+<?
+
+// PHP Data Objects(PDO) Sample Code:
+
+//extension_dir = "\SQLSRV510";
+
+//extension=php_sqlsrv_72_ts.dll;
+
+//extension=php_pdo.dll;
+
+$ip = $_POST["ips"];
+$button = $_POST["button"];
+$time = $_POST["time"];
+
+$serverName = "jbsdatatest.database.windows.net";  
+$connectionOptions = array(  
+    "Database" => "JBsDataTest",  
+    "UID" => "jbmoneysweg",  
+    "PWD" => "Jeremiah72*"  
+);  
+$conn = sqlsrv_connect($serverName, $connectionOptions);  
+  
+
+if ($conn === false)  
+    {  
+    die(print_r(sqlsrv_errors() , true));  
+    }  
+
+
+        /*Insert data.*/  
+        $insertSql = "INSERT INTO test6 (Ip,button,time)   
+VALUES (?,?,?,?)";  
+        $params = array($ip, $button, $time  
+        );  
+        $stmt = sqlsrv_query($conn, $insertSql, $params);  
+        if ($stmt === false)  
+            {  
+            /*Handle the case of a duplicte e-mail address. */ 
+            $errors = sqlsrv_errors();  
+            if ($errors[0]['code'] == 2601)  
+                {  
+                echo "The e-mail address you entered has already been used.</br>";  
+                }  
+  
+            /*Die if other errors occurred.*/  
+              else  
+                {  
+                die(print_r($errors, true));  
+                }  
+            }  
+          else  
+            {  
+            echo "Registration complete.</br>";  
+            }  
+?>
