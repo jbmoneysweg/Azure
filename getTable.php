9176186eb324f86deafc21061a26e3c39613e711
jbmoneysweg <87237420+jbmoneysweg@users.noreply.github.com>
2022-09-08 10:43:49 -0400

    Update getTable.php

--- getTable.php
+++ getTable.php
@@ -1,63 +1,13 @@
-function compare($a,$b){
+<?php
+    header('Content-Type: application/json');
 
-$ip = $_POST["ipAdd"];
-$locations = $_POST["locations"];
-$visit = $_POST["visit"];
-$pgOne = $_POST["pgOne"];
-$visit = 0;
-$time = 0.0;
-$value = 0.0;
-$sendback = "";
-$table = "";
-$password1 = "Jeremiah72*";
-$pass;
-
-if ($a == 1) {
-    $pass = $password1;
+    function compare($a,$b){
+  $c=$a+$b;
+  return $c;
 }
 
 
-$serverName = "jbsdatatest.database.windows.net";  
-$connectionOptions = array(  
-    "Database" => "JBsDataTest",  
-    "UID" => "jbmoneysweg",  
-    "PWD" => "Jeremiah72*"  
-);  
-$conn = sqlsrv_connect($serverName, $connectionOptions);  
-  
-
-if ($conn === false)  
-    {  
-    die(print_r(sqlsrv_errors() , true));  
-    }  
-
-
-$sql = "SELECT * FROM test8 WHERE password=" + $pass; 
-//$sql = "SELECT * FROM test5"; 
-$stmt = sqlsrv_query($conn, $sql); 
-if($stmt === false) 
-{ 
-die(print_r(sqlsrv_errors(), true)); 
-} 
- 
-if(sqlsrv_has_rows($stmt)) 
-{ 
-    /*
-print("<table border='1px'>"); 
-print("<tr><td>Emp Id</td>"); 
-print("<td>Name</td>"); 
-print("<td>education</td>"); 
-print("<td>Email</td></tr>"); 
- */
-while($row = sqlsrv_fetch_array($stmt)) //!= NULL
-{ 
-$table = $row[1];
-} 
-  return $table;
-}
-}
-
-$aResult = array();
+    $aResult = array();
 
     if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
 
@@ -71,7 +21,7 @@ $aResult = array();
                    $aResult['error'] = 'Error in arguments!';
                }
                else {
-                   $aResult['result'] = compare(intval($_POST['arguments'][0]), intval($_POST['arguments'][1]));
+                   $aResult['result'] = compare(floatval($_POST['arguments'][0]), floatval($_POST['arguments'][1]));
                }
                break;
 
@@ -84,5 +34,4 @@ $aResult = array();
 
     echo json_encode($aResult);
 
-//$visit = intval($visit);
 ?>
