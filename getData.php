6cff9601612565e237a3eb1c2f07f041c927b0dc
jbmoneysweg <87237420+jbmoneysweg@users.noreply.github.com>
2022-09-08 13:30:43 -0400

    Update getData.php

--- getData.php
+++ getData.php
@@ -1,60 +1,61 @@
-function compare($a,$b){
+ <?php
+    
 
-$visit = 0;
-$time = 0.0;
-$value = 0.0;
-$sendback = "";
-$Ip = array();
-$locations = array();
-$visit = array();
-$page = array();
-$carrier = array();
-
-$serverName = "jbsdatatest.database.windows.net";  
-$connectionOptions = array(  
-    "Database" => "JBsDataTest",  
-    "UID" => "jbmoneysweg",  
-    "PWD" => "Jeremiah72*"  
-);  
-$conn = sqlsrv_connect($serverName, $connectionOptions);  
+    function compare($a,$b){
+        $c = $b;
+        $d = "test5";
+        $sql;
+        $Ip = array();
+        $locations = array();
+        $visit = array();
+        $page = array();
+        $carrier = array();
+            
+        $serverName = "jbsdatatest.database.windows.net";  
+        $connectionOptions = array(  
+            "Database" => "JBsDataTest",  
+            "UID" => "jbmoneysweg",  
+            "PWD" => "Jeremiah72*"  
+        );  
+        $conn = sqlsrv_connect($serverName, $connectionOptions);  
   
 
-if ($conn === false)  
-    {  
-    die(print_r(sqlsrv_errors() , true));  
-    }  
+        if ($conn === false)  
+            {  
+            die(print_r(sqlsrv_errors() , true));  
+            }  
 
 
-$sql = "SELECT * FROM " + $a; 
-//$sql = "SELECT * FROM test5"; 
-$stmt = sqlsrv_query($conn, $sql); 
-if($stmt === false) 
-{ 
-die(print_r(sqlsrv_errors(), true)); 
-} 
+        if ($a == $d) {
+            $sql = "SELECT * FROM test5";  
+        } else {
+            return "error";
+        }
+        
+
+        $stmt = sqlsrv_query($conn, $sql); 
+        if($stmt === false) 
+            { 
+            die(print_r(sqlsrv_errors(), true)); 
+            } 
  
-if(sqlsrv_has_rows($stmt)) 
-{ 
-    /*
-print("<table border='1px'>"); 
-print("<tr><td>Emp Id</td>"); 
-print("<td>Name</td>"); 
-print("<td>education</td>"); 
-print("<td>Email</td></tr>"); 
- */
-while($row = sqlsrv_fetch_array($stmt)) //!= NULL
-{ 
-array_push($Ip, $row[1]);
-array_push($locations, $row[2]);
-array_push($visit, $row[3]);
-array_push($page, $row[4]);
-} 
-array_push($carrier, $Ip, $locations, $visit, $page);
-  return $carrier;
-}
-}
+        if(sqlsrv_has_rows($stmt)) 
+        { 
+            while($row = sqlsrv_fetch_array($stmt)) //!= NULL
+                { 
+                array_push($Ip, $row[1]);
+                array_push($locations, $row[2]);
+                array_push($visit, $row[3]);
+                array_push($page, $row[4]);
+                } 
+            array_push($carrier, $Ip, $locations, $visit, $page);
+            return $carrier;
+  
+        }
+    }
 
-$aResult = array();
+
+    $aResult = array();
 
     if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
 
@@ -68,7 +69,7 @@ $aResult = array();
                    $aResult['error'] = 'Error in arguments!';
                }
                else {
-                   $aResult['result'] = compare(($_POST['arguments'][0]), ($_POST['arguments'][1]));
+                   $aResult['result'] = compare(strval($_POST['arguments'][0]), floatval($_POST['arguments'][1]));
                }
                break;
 
@@ -81,5 +82,4 @@ $aResult = array();
 
     echo json_encode($aResult);
 
-//$visit = intval($visit);
 ?>
