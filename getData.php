3a980b72488e66a9b0e9ca0dfa09e38e19f85086
jbmoneysweg <87237420+jbmoneysweg@users.noreply.github.com>
2023-04-18 09:47:09 -0400

    Update getData.php

--- getData.php
+++ getData.php
@@ -45,7 +45,7 @@
                 {
                 array_push($Ip, $row[0]);
                 array_push($locations, $row[1]);
-                array_push($visit, $row[2]."*".$row[5]);
+                array_push($visit, $row[2]."~".$row[5]);
                 array_push($page, $row[3]);
                 array_push($business, $row[4]);
                 
